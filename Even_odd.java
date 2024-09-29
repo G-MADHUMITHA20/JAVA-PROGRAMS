3f6c632a2eab93846a89724a80288877d0d93ef9
G.Madhumitha. <gmadhumitha1357@gmail.com>
2024-09-29 11:22:02 +0530

    Add files via upload

---
+++ Even_odd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class check
+{ public static void main(String[] args)
+{
+    Scanner input=new Scanner(System.in);
+    System.out.println("ENTER NUMBER:");
+    int n=input.nextInt();
+    if(n%2==0)
+        {
+            
+         System.out.println("Even number");
+         
+        }
+    else
+    
+    {
+          System.out.println("Odd number");
+    }
+        
+}}
